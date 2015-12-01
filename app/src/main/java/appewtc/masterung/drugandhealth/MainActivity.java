44d346f82243a2cb54cd8bfd9f1560ffcf7f18e8
Sophon <sophonphubunlap@gmail.com>
2015-12-01 16:59:54 +0700

    Start

---
+++ app/src/main/java/appewtc/masterung/drugandhealth/MainActivity.java
@@ -0,0 +1,37 @@
+package appewtc.masterung.drugandhealth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ManageTABLE objManageTABLE;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Request Database
+        objManageTABLE = new ManageTABLE(this);
+
+    }   // onCreate
+
+    public void clickDrug(View view) {
+        startActivity(new Intent(MainActivity.this, DrugActivity.class));
+    }
+
+    public void clickHerb(View view) {
+       startActivity(new Intent(MainActivity.this, HerbActivity.class));
+    }
+
+    public void clickPersonal(View view) {
+        startActivity(new Intent(MainActivity.this, PersonalActivity.class));
+    }
+
+
+}   // Main Class
